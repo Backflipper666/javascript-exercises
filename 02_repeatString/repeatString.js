f9bb63b142feed125c2fbe8f50d81a9d1064633d
Backflipper666 <elderly619@gmail.com>
2022-02-28 00:08:07 +0600

    done 4th 1st test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = '';
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < string.length; j++) {
+            output += string[j];
+        }
+        
+    }
+    return output;
 };
-
+//
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    let output = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == num) {
+            continue;
+        }
+        output.push(array[i]);
+    }
+    return output;
 };
 
 // Do not edit below this line
