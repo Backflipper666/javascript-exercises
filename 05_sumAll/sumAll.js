1fa0c882f27b8832fcbdb8bbe20a022af77dcdab
Backflipper666 <elderly619@gmail.com>
2022-03-02 02:03:42 +0600

    sumAll file passes all 6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,19 @@
 const sumAll = function(a, b) {
-    if ( a < 0 || b < 0) {
-        throw Error();
+    if (typeof(a) == "string" || typeof(b) == "string" || typeof(a) == "object" || typeof(b) == "object" ) {
+        return "ERROR";
+   }
+    else {
+        if ( a < 0 || b < 0) return "ERROR";
+    
+    if (b < a) {
+        let output = 0;
+        while (b <= a) {
+            output = output + b;
+            b++;
+        }
+        return output;
     }
-    if (a < b) {
+    else if (a < b) {
         let output = 0;
         while(a <= b){
             output = output + a;
@@ -12,7 +23,7 @@ const sumAll = function(a, b) {
         return output;
 
     }
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
