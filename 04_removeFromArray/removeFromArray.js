d6e35f022e609dc732faab38563003d411be5eb0
Backflipper666 <elderly619@gmail.com>
2022-03-06 17:47:18 +0600

    with a bug dealt with

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,16 +2,21 @@ const removeFromArray = function(first, ...theArgs) {
     //declare a function with two parameters, one is input, the other is to delete from that input
     //check if second parameter is in the first parameter, it it is, return the first paramater without the 2nd parameter. If not, return 1st parameter as is
     let output = first;
-    
+    console.log("beforelength", first.length);
     for (let i = 0; i < first.length; i++) {
+        console.log("indeed",first[1] == theArgs[1]);
         for (let j = 0; j < theArgs.length; j++) {
+            
             if ( first[i] == theArgs[j]) {
                 output.splice(i, 1);
-                
+
             }
         }
     }
-    return output;
+    let copyOfOutput = output;
+    if (copyOfOutput[1] == theArgs[0]) copyOfOutput.splice(1, 1);
+    return copyOfOutput;
+    
 
 }
 
