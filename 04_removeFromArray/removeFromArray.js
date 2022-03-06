5cdaa56583f82e6606de2d60dd9bf3b044039729
Backflipper666 <elderly619@gmail.com>
2022-03-06 07:00:40 +0600

    index.html
    removes elements but throws error when [1, 2, 3, 4], 3, 2. Expected [1, 4], throws [1, 3, 4]

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,19 @@
-const removeFromArray = function(array, num, num2) {
-    let output = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] == num || array[i] == num2) {
-            continue;
+const removeFromArray = function(first, ...theArgs) {
+    //declare a function with two parameters, one is input, the other is to delete from that input
+    //check if second parameter is in the first parameter, it it is, return the first paramater without the 2nd parameter. If not, return 1st parameter as is
+    let output = first;
+    
+    for (let i = 0; i < first.length; i++) {
+        for (let j = 0; j < theArgs.length; j++) {
+            if ( first[i] == theArgs[j]) {
+                output.splice(i, 1);
+                
+            }
         }
-        output.push(array[i]);
     }
     return output;
-};
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
