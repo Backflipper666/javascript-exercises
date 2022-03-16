11c6adbc447242e4783a8ce6131a9ffbbfffe017
Backflipper666 <elderly619@gmail.com>
2022-03-16 11:25:04 +0600

    convert roman numerals to int, but convert operators too

--- calc/calculator.js
+++ calc/calculator.js
@@ -1,36 +1,41 @@
 function calculator(string) {
-    // код пишите внутри этой функции
-              let output = "";
-             
-              let operator = string[2];
-              let firstOperand = parseInt(string[0]);
-              let secOperand = parseInt(string[4]);
-              if (operator === "+") {
+  const noWsStr = string.replace(/\s/g, '');
+  const operators = noWsStr.replace(/[\d.,]/g, '').split('');
+  const operands = noWsStr.replace(/[+/%*-]/g, ' ')
+                          .replace(/\,/g, '.')
+                          .split(' ')
+                          .map(parseFloat)
+                          .filter(it => it);
 
-                  output = firstOperand + secOperand;
-                  return ((output.toString()));
-              }
-              else if (operator === "-") {
-                output = firstOperand - secOperand;
-                output = output.toString();
-                return output;
-              }
-              else if (operator === "*") {
-                  output = firstOperand * secOperand;
-                  output = output.toString();
-                  return output;
-              }
-              else if (operator === "/") {
-                  output = firstOperand / secOperand;
-                  output = output.toString();
-                  return output;
-              }
+  if (operators.length >= operands.length){
+      throw new Error('Operators qty must be lesser than operands qty')
+  };
 
-  
-             
-    
+  while (operators.includes('*')) {
+      let opIndex = operators.indexOf('*');
+      operands.splice(opIndex, 2, operands[opIndex] * operands[opIndex + 1]);
+      operators.splice(opIndex, 1);
+  };
+  while (operators.includes('/')) {
+      let opIndex = operators.indexOf('/');
+      operands.splice(opIndex, 2, operands[opIndex] / operands[opIndex + 1]);
+      operators.splice(opIndex, 1);
+  };
+  while (operators.includes('%')) {
+      let opIndex = operators.indexOf('%');
+      operands.splice(opIndex, 2, operands[opIndex] % operands[opIndex + 1]);
+      operators.splice(opIndex, 1);
+  };
+
+  let result = operands[0];
+  for (let i = 0; i < operators.length; i++) {
+      operators[i] === '+' ? (result += operands[i + 1]) : (result -= operands[i + 1])
   }
-  console.log(calculator("3 - 1"));
+  let rounded = Math.floor(result);
+  let resultConverted = rounded.toString();
+  return resultConverted;
+  }
+  console.log(calculator("10 - 1"));
   console.log(typeof(output));
   
   module.exports = calculator; // Не трогайте эту строчку
---
+++ calc/script.js
@@ -0,0 +1,115 @@
+
+function calculator(string) {
+    const noWsStr = string.replace(/\s/g, '');
+    const operators = noWsStr.replace(/[\d.,]/g, '').split('');
+    const operands = noWsStr.replace(/[+/%*-]/g, ' ')
+                            .replace(/\,/g, '.')
+                            .split(' ')
+                            .map(parseFloat)
+                            .filter(it => it);
+
+    if (operators.length >= operands.length){
+        throw new Error('Operators qty must be lesser than operands qty')
+    };
+
+    while (operators.includes('*')) {
+        let opIndex = operators.indexOf('*');
+        operands.splice(opIndex, 2, operands[opIndex] * operands[opIndex + 1]);
+        operators.splice(opIndex, 1);
+    };
+    while (operators.includes('/')) {
+        let opIndex = operators.indexOf('/');
+        operands.splice(opIndex, 2, operands[opIndex] / operands[opIndex + 1]);
+        operators.splice(opIndex, 1);
+    };
+    while (operators.includes('%')) {
+        let opIndex = operators.indexOf('%');
+        operands.splice(opIndex, 2, operands[opIndex] % operands[opIndex + 1]);
+        operators.splice(opIndex, 1);
+    };
+
+    let result = operands[0];
+    for (let i = 0; i < operators.length; i++) {
+        operators[i] === '+' ? (result += operands[i + 1]) : (result -= operands[i + 1])
+    }
+    let rounded = Math.floor(result);
+    let resultConverted = rounded.toString();
+    return resultConverted;
+}
+
+function convertToRoman(num) {
+    var roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    var str = '';
+
+    for (var i of Object.keys(roman)) {
+        var q = Math.floor(num / roman[i]);
+        num -= q * roman[i];
+        str += i.repeat(q);
+    }
+
+    return str;
+}
+        
+
+function roman_to_Int(str1) {
+    if(str1 == null) return -1;
+    var num = char_to_int(str1.charAt(0));
+    var pre, curr;
+
+    for(var i = 1; i < str1.length; i++){
+    curr = char_to_int(str1.charAt(i));
+    pre = char_to_int(str1.charAt(i-1));
+    if(curr <= pre){
+    num += curr;
+    } else {
+    num = num - pre*2 + curr;
+    }
+    }
+
+    return num;
+    }
+
+    function char_to_int(c){
+    switch (c){
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default: return -1;
+    }
+}
+
+
+
+
+//split string by " ";
+//replace operands by "",
+function calc(roman) {
+    let operator = roman.split(" ");
+
+    for (let i = 0; i < operator.length; i++) {
+        if (operator != "+" || operator != "-" || operator != "/" || operator != "*") {
+            roman_to_Int(operator[i]);
+            console.log(roman_to_Int(operator[i]));
+        }
+    }
+
+} 
+calc("XX + X");
