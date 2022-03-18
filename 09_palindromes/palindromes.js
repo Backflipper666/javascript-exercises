80674ed4ad45308a33ef3c82dad64663ffbb4cc5
Backflipper666 <elderly619@gmail.com>
2022-03-18 23:22:55 +0600

    check if the word is palindrome, works for only one word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,53 @@
-const palindromes = function () {
-
+const asIs = function (string) {
+    //take string, remove punctuations, make lowercase
+    //take string backwards, remove punctuation, make lowercase
+    //compare them
+    //split long string by space and compare them
+    //omit "!", "?", ".", ","
+    let strArray = string.split("");
+        for (let i = 0; i < strArray.length; i++) {
+            if ("!" === strArray[i]) {
+                strArray.splice(i, 1);
+            }
+        }
+        let stringJoined = strArray.join("");
+    return stringJoined.toLowerCase();
 };
+
+
+
+
+function reverseString(string) {
+    let output = "";
+    for (let i = string.length - 1; i >= 0; i--){
+        output += string[i];
+    }
+    let lowerCased = output.toLowerCase();
+    return removePunctuation(lowerCased);
+}
+
+
+
+function removePunctuation(string) {
+    let myArray = string.split("");
+    for (let i = 0; i < myArray.length; i++) {
+        if ("!" === myArray[i] || "?" === myArray[i] || "." == myArray[i]) {
+            myArray.splice(i, 1);
+        }
+    }
+    let strJoined = myArray.join("");
+
+    return strJoined;
+}
+
+const palindromes = function(string) {
+    if (asIs(string) === reverseString(string)) return true;
+    else {
+        return false;
+    }
+}
+        
+     
 
 // Do not edit below this line
 module.exports = palindromes;
